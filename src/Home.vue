119490bc8879719206905a68a7d8e112ccebf01b
Marina Feller <marifeller523@gmail.com>
2025-12-05 16:28:25 -0700

    Rewrote project base code

---
+++ src/Home.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <header>
+      <h1>Marina Feller</h1>
+    </header>
+    <section></section>
+  </main>
+</template>
+
+<style scoped>
+main {
+  position: fixed;
+  background-color: blue;
+  width: 100%;
+  height: 100%;
+  border: 0px;
+  margin: 0px;
+  padding: 0px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import './assets/main.css'
+import './styles.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import Home from './Home.vue'
 
-createApp(App).mount('#app')
+createApp(Home).mount('#app')
